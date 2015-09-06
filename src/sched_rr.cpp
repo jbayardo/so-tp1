db83c426164ac4bf3982c2a491a17e08fdbd287d
Julian Bayardo <julian@bayardo.com.ar>
2015-09-06 19:27:58 -0300

    Todo el codigo comentado

--- src/sched_rr.cpp
+++ src/sched_rr.cpp
@@ -7,14 +7,16 @@
 using namespace std;
 
 SchedRR::SchedRR(vector<int> argn) {
-	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+	// Ponemos la cantidad de cores que tenemos
 	this->cores = argn[0];
 
 	for (int i = 1; i < argn.size(); ++i) {
+		// Inicializamos todos los cores a correr la IDLE, y ponemos el tamaño de cada quantum
 		this->quantums.push_back(argn[i]);
 		this->coreStates[i-1] = IDLE_TASK;
 	}
 
+	// Da IDLE is running, nigga!
 	running[IDLE_TASK] = 1;
 }
 
@@ -24,12 +26,12 @@ SchedRR::~SchedRR() {
 
 
 void SchedRR::load(int pid) {
-	// Llega una tarea
+	// Llega una tarea, asi que la ponemos en la cola global
 	this->next.push(pid);
 }
 
 void SchedRR::unblock(int pid) {
-	// Se desbloquea una tarea
+	// Se desbloquea una tarea, asi que la volvemos a poner en la cola global
 	this->next.push(pid);
 }
 
@@ -37,28 +39,37 @@ int SchedRR::tick(int cpu, const enum Motivo m) {
 	// Tick de CPU
 	switch (m) {
 		case TICK: {
+			// Obtenemos el PID actual
 			int pid = coreStates[cpu];
+
+			// Forzamos el quantum a 1, asi si estamos en la IDLE hacemos context switch
 			int quantum = 1;
 
 			if (pid != IDLE_TASK) {
+				// Como no estamos en la IDLE, ponemos el quantum apropiado
 				quantum = running[pid];
 			}
 
+			// Lo decrementamos
 			--quantum;
 
-			// Si termino el quantum
 			if (quantum == 0) {
+				// Si termino el quantum, buscamos que podemos hacer
 				if (next.empty()) {
+					// No queda nada más para procesar, asi que le renovamos el quantum!
 					running[pid] = quantums[cpu];
 					return pid;
 				} else {
 					if (pid != IDLE_TASK) {
+						// Volvemos a ponerlo en la cola, para schedulearlo globalmente
 						next.push(pid);
 					}
 
+					// Obtenemos el proximo proceso
 					int nextPid = next.front();
 					next.pop();
 
+					// Inicializamos todo lo necesario!
 					running.erase(pid);
 					running[nextPid] = quantums[cpu];
 					coreStates[cpu] = nextPid;
@@ -66,22 +77,29 @@ int SchedRR::tick(int cpu, const enum Motivo m) {
 					return nextPid;
 				}
 			} else {
+				// Seguimos corriendo, solo actualizamos cuanto nos queda
 				running[pid] = quantum;
 				return pid;
 			}
 
 			break;
 		}
+		// Bloquear es practicamente lo mismo que salir, para nosotros
 		case BLOCK:
 		case EXIT: {
+			// Obtenemos el PID actual
 			int pid = coreStates[cpu];
+
+			// Nos aseguramos de no usarlo más
 			running.erase(pid);
 
 			if (next.empty()) {
+				// Si no queda otra, nos vamos a la IDLE
 				coreStates[cpu] = IDLE_TASK;
 
 				return IDLE_TASK;
 			} else {
+				// Si no, obtenemos el próximo proceso!
 				int nextPid = next.front();
 				next.pop();
 
--- src/sched_rr2.cpp
+++ src/sched_rr2.cpp
@@ -7,13 +7,21 @@
 using namespace std;
 
 SchedRR2::SchedRR2(vector<int> argn) {
-	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+	// Ponemos la cantidad de cores
 	this->ncores = argn[0];
 
+	// Inicializamos las estructuras
 	for (int i = 1; i < argn.size(); ++i) {
+		// Todos empiezan sin procesos corriendo
 		this->total.push_back(0);
+
+		// Cargamos el quantum correspondiente
 		this->quantums.push_back(argn[i]);
+
+		// Una cola vacia por procesar
 		this->queues.push_back(std::queue<int>());
+
+		// La idle empezando desde 0
 		this->ticks.push_back(0);
 		this->running.push_back(IDLE_TASK);
 	}
@@ -27,34 +35,46 @@ SchedRR2::~SchedRR2() {
 void SchedRR2::load(int pid) {
 	int minimum = 0;
 
+	// Buscamos el procesador con menor numero de procesos totales
 	for (int i = 1; i < this->ncores; ++i) {
 		if (this->total[i] < total[minimum]) {
 			minimum = i;
 		}
 	}
 
+	// Aumentamos la cantidad de procesos totales
 	this->total[minimum]++;
+
+	// Lo ponemos en la cola del procesador
 	this->queues[minimum].push(pid);
 }
 
 void SchedRR2::unblock(int pid) {
+	// Buscamos en que procesador estaba el pid
 	auto it = this->blocked.find(pid);
 
 	if (it != this->blocked.end()) {
+		// Lo ponemos en la cola del procesador
 		this->queues[it->second].push(pid);
+
+		// Lo borramos permanentemente
 		this->blocked.erase(pid);
 	}
 }
 
 int SchedRR2::tick(int cpu, const enum Motivo m) {
+	// Buscamos el PID corriendo en el CPU actual
 	int pid = this->running[cpu];
 
 	switch(m) {
 		case TICK:
+			// Aumentamos los ticks
 			++this->ticks[cpu];
 
 			if (this->running[cpu] == IDLE_TASK) {
+				// Si estamos en la IDLE...
 				if (!this->queues[cpu].empty()) {
+					// Y tenemos otra cosa para ejecutar, cambiamos de contexto
 					this->ticks[cpu] = 0;
 
 					int next = this->queues[cpu].front();
@@ -63,7 +83,10 @@ int SchedRR2::tick(int cpu, const enum Motivo m) {
 					this->running[cpu] = next;
 				}
 			} else {
+				// No estamos en la idle
+
 				if (this->ticks[cpu] >= this->quantums[cpu]) {
+					// Nos pasamos del quantum!, cambiamos de contexto
 					this->queues[cpu].push(pid);
 					this->ticks[cpu] = 0;
 
@@ -73,14 +96,20 @@ int SchedRR2::tick(int cpu, const enum Motivo m) {
 					this->running[cpu] = next;
 				}
 			}
+
 			break;
 		case BLOCK:
+			// Nos bloqueamos
 			this->blocked[pid] = cpu;
+
+			// Reseteamos el quantum
 			this->ticks[cpu] = 0;
 
 			if (this->queues[cpu].empty()) {
+				// No hay nada para seguir, vamos a la idle
 				this->running[cpu] = IDLE_TASK;
 			} else {
+				// Cambio a otra tarea!
 				int next = this->queues[cpu].front();
 				this->queues[cpu].pop();
 
@@ -89,12 +118,17 @@ int SchedRR2::tick(int cpu, const enum Motivo m) {
 
 			break;
 		case EXIT:
+			// Bajamos los procesos corriendo
 			this->total[cpu]--;
+
+			// Reseteamos el quantum
 			this->ticks[cpu] = 0;
 
 			if (this->queues[cpu].empty()) {
+				// No hay nada para seguir, vamos a la idle
 				this->running[cpu] = IDLE_TASK;
 			} else {
+				// Cambio a otra tarea!
 				int next = this->queues[cpu].front();
 				this->queues[cpu].pop();
 
